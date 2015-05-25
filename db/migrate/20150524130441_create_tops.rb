808b62a4afa139f8b14f0a85f7db3d75e8a4703d
Mun <anstjddlsdl@naver.com>
2015-05-24 21:12:52 -0700

    Mun branch( Catalog and Image Detail Infomation added 2.

---
+++ db/migrate/20150524130441_create_tops.rb
@@ -0,0 +1,25 @@
+class CreateTops < ActiveRecord::Migration
+  def change
+    create_table :tops do |t|
+      t.string :main_title
+      t.text :description
+      #t.string :size
+      t.decimal :stock
+      #t.string :color
+
+
+      t.string :main_image_url		# 메인 이미지
+
+      t.string :image_url1		# 모든 색상 이미지
+
+      t.string :image_url2		# 착용 이미지1
+      t.string :image_url3		# 착용 이미지2
+      t.string :image_url4		# 착용 이미지3
+      t.string :image_url5		# 착용 이미지4		
+
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps null: false
+    end
+  end
+end
